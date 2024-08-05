c102b6ba0265c70f61ae9ce2b401bd19b7d02b9a
Muhammad Farhan <farhanzafrani@gmail.com>
2024-08-05 23:23:51 +0500

    python files updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,22 +5,23 @@ import zipfile
 from logger.logging import Logger
 from utils.common import get_size
 from config.configuration import ConfigurationManager
+from entity.config_entity import DataIngestionConfig
 
 
 class DataIngestion:
-    def __init__(self, config):
+    def __init__(self, config: DataIngestionConfig):
         self.config = config
 
     def download_file(self):
         if not os.path.exists(self.config.local_data_file):
             filename, headers = request.urlretrieve(
-                url = self.config.source_URL,
-                filename = self.config.local_data_file
+                url=self.config.source_URL, filename=self.config.local_data_file
             )
             Logger.info(f"{filename} download! with following info: \n{headers}")
         else:
-            Logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
-
+            Logger.info(
+                f"File already exists of size: {get_size(Path(self.config.local_data_file))}"
+            )
 
     def extract_zip_file(self):
         """
@@ -30,9 +31,8 @@ class DataIngestion:
         """
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
-        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+        with zipfile.ZipFile(self.config.local_data_file, "r") as zip_ref:
             zip_ref.extractall(unzip_path)
-
 
 
 def main():
@@ -44,7 +44,7 @@ def main():
         data_ingestion.extract_zip_file()
     except Exception as e:
         raise e
-    
+
 
 if __name__ == "__main__":
     main()
